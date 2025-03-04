0a6e56459a00ac81c9e2a7cf73046631df291f8f
Dortey Tetteh <emmanueltdortey@yahoo.com>
2025-03-04 02:54:52 +0000

    added code for increament in the console

--- Scrimba/Build a passenger counter app/index.js
+++ Scrimba/Build a passenger counter app/index.js
@@ -1,21 +1,15 @@
 // document.getElementById("count").innerText = 5
 
-// let count = 5
-// count = count + 1
-// console.log(count)
 
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
-// Console.log the value after each step
 
-let bonusPoints = 50
-console.log(bonusPoints)
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
 
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
+let count  = 0;
 
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+function increament() {
+    count = count + 1
+    console.log(count)
+}
