f49494c5c2e1b1c4e46498312fe37750e9d593fd
Dortey Tetteh <emmanueltdortey@yahoo.com>
2025-03-05 07:42:49 +0000

    made some debuging to make the code work.

--- Scrimba/Basketball Scoreboard - Solo project/script.js
+++ Scrimba/Basketball Scoreboard - Solo project/script.js
@@ -1,4 +1,6 @@
-let scoreScreen = document.getElementById("score-screen")
+let homeScoreScreen = document.getElementById("home-score-screen")
+
+let awayScoreScreen = document.getElementById("away-score-screen")
 
 // let homePlusOne = document.getElementById("home-plus-one")
 // let homePlusTwo = document.getElementById("home-plus-two")
@@ -7,26 +9,49 @@ let scoreScreen = document.getElementById("score-screen")
 
 
 let score = 0
+let score2 = 0
 
 function homePlusOne() {
     score += 1
     // console.log(score)
-    scoreScreen.innerText = score
+    homeScoreScreen.innerText = score
 }
 
 function homePlusTwo() {
     score += 2
     // console.log(score)
-    scoreScreen.innerText = score
+    homeScoreScreen.innerText = score
 }
 
 function homePlusThree() {
     score += 3
     // console.log(score)
-    scoreScreen.innerText = score
+    homeScoreScreen.innerText = score
+}
+
+
+
+function awayPlusOne() {
+    score2 += 1
+    // console.log(score)
+    awayScoreScreen.innerText = score2
+}
+
+function awayPlusTwo() {
+    score2 += 2
+    // console.log(score)
+    awayScoreScreen.innerText = score2
+}
+
+function awayPlusThree() {
+    score2 += 3
+    // console.log(score)
+    awayScoreScreen.innerText = score2
 }
 
 function resetScore() {
     score = 0
-    scoreScreen.innerText = score
+    score2 = 0
+    homeScoreScreen.innerText = score
+    awayScoreScreen.innerText = score2
 }
