b20255faad1a85494da6892e202eb77f2a6767be
Dortey Tetteh <emmanueltdortey@yahoo.com>
2025-03-05 07:11:56 +0000

    made some changes to the exixting js code and also added a rest button and some code to make it work

--- Scrimba/Basketball Scoreboard - Solo project/script.js
+++ Scrimba/Basketball Scoreboard - Solo project/script.js
@@ -1,26 +1,32 @@
 let scoreScreen = document.getElementById("score-screen")
 
-let homePlusOne = document.getElementById("home-plus-one")
-let homePlusTwo = document.getElementById("home-plus-two")
-let homePlusThree = document.getElementById("home-plus-two")
+// let homePlusOne = document.getElementById("home-plus-one")
+// let homePlusTwo = document.getElementById("home-plus-two")
+// let homePlusThree = document.getElementById("home-plus-two")
+
 
 
 let score = 0
 
-function PlusOne() {
+function homePlusOne() {
     score += 1
     // console.log(score)
     scoreScreen.innerText = score
 }
 
-function PlusTwo() {
+function homePlusTwo() {
     score += 2
     // console.log(score)
     scoreScreen.innerText = score
 }
 
-function PlusThree() {
+function homePlusThree() {
     score += 3
     // console.log(score)
+    scoreScreen.innerText = score
+}
+
+function resetScore() {
+    score = 0
     scoreScreen.innerText = score
 }
