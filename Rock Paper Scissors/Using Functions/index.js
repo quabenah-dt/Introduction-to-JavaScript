b4eced3b6e182e848d95a6e7cd4cfff789ec1a78
Dortey Tetteh <emmanueltdortey@yahoo.com>
2025-03-07 01:31:20 +0000

    made some changes to my initial code.

--- Rock Paper Scissors/Using Functions/index.js
+++ Rock Paper Scissors/Using Functions/index.js
@@ -10,3 +10,18 @@
 // - if between 0 and 1/2 => rock
 // - if between 1/3 and 2/3 => paper
 // - if between 2/3 and 1 => scissors
+
+
+
+const randomNumber = Math.random();
+let computerMove = "";
+
+function playGame() {
+    if (randomNumber >= 0 &&  randomNumber <  1/2) {
+        console.log("Paper");
+    } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+        console.log("Rock");
+    } else if  (randomNumber >= 2/3 && randomNumber < 1 ) {
+        console.log("Scissors");
+    }
+}
